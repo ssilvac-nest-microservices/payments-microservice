418207c6c8f49c7d752806e042eef9a1b0f95edd
Sebastián Silva <sebasilvac88@gmail.com>
2024-04-09 00:24:23 -0400

    init configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Payments');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+
+  logger.log(`Payments microservice is running on PORT: ${envs.port}`);
 }
 bootstrap();
